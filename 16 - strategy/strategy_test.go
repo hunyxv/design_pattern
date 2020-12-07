0124535f3afd2738454047f041b9957004ed6522
hunyxv <1035794358@qq.com>
2020-12-07 15:00:17 +0800

    fix: append

--- 16 - strategy/strategy_test.go
+++ 16 - strategy/strategy_test.go
@@ -10,20 +10,19 @@ func TestRemoveRepByMap(t *testing.T) {
 	slc := make([]int, 100000)
 	r := rand.New(rand.NewSource(time.Now().UnixNano()))
 
-	for i:=0; i< 100000; i++ {
-		slc= append(slc, r.Intn(100000))
+	for i := 0; i < 100000; i++ {
+		slc[i] = r.Intn(100000)
 	}
 	result := RemoveRep(slc)
 	t.Log(len(result))
 }
 
-
 func TestRemoveRepByLoop(t *testing.T) {
 	slc := make([]int, 1000)
 	r := rand.New(rand.NewSource(time.Now().UnixNano()))
 
-	for i:=0; i< 1000; i++ {
-		slc= append(slc, r.Intn(1000))
+	for i := 0; i < 1000; i++ {
+		slc[i] = r.Intn(1000)
 	}
 	result := RemoveRep(slc)
 	t.Log(len(result))
